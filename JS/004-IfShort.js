797314e0a61122dc7de0ba1207b047efcf80f381
Hugo GG <mategomez04.hg@gmail.com>
2023-09-20 10:27:39 -0600

    Agregue ejercicios de IfShort con JS

---
+++ JS/004-IfShort.js
@@ -0,0 +1,34 @@
+/*EJERCICIO DOS
+let calificacion = 59.9;
+let examen = calificacion >= 60 ? "Aprobado" : "Reprobado";
+console.log(examen)
+*/
+
+
+/*EJERICIO TRES:
+Imprimir la calificacion cuando el alumno saque 95 o mas poner A+ 
+si el alumno saca 90 o mas poner solo A 
+si el alumno saca un 85 o mas B+
+si el alumno saca 80 o mas solo B
+si el alumno saca 75 o mas solo C+
+si el alumno saca 70 o mas solo C
+si el alumno saca 65 o mas D+
+si el alumno saca 60 o mas D
+si solo saca de 59 o menos F
+*/
+
+
+/* EJERCICIO UNO INICIAR SESION CON UN IF SHORT
+let login = "holamundo";
+console.log(login === true ? "Pude iniciar sesion" : "No pude entrar")
+*/
+
+
+
+/*let numero = 11;
+let num = "11";
+if (numero === num) {
+    console.log("los datos son iguales")
+}else{
+    console.log("Son diferentes")
+}*/
