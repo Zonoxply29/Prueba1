646f7eedeb88578cfd20a440896c2edca30f6e50
Hugo GG <mategomez04.hg@gmail.com>
2023-09-20 12:03:01 -0600

    Aprendi Bucle For

--- JS/005-For.js
+++ JS/005-For.js
@@ -10,9 +10,21 @@ for (let index = 0; index <11 ; index++) {
     const element = index;
     console.log(element)
 }
-
-*/
-
+ CODIGO RESUMIDO
 for ( i = 0; i < 11; i++) {
 console.log(i);    
 }
+*/
+
+let paises = ["Mexico","Guatemala","Brazil","Chile","Argentina"];
+
+for( i=0; i <= 10; i++){
+console.log(paises[i])
+}
+
+/*
+RESTANDO CON UN BUCLE
+for ( i = 15; i > 4; i--) {
+console.log(i)    
+}
+*/
